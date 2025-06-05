b40d95e74e936900b6f8990eaf1f76696d411c6e
grbll@posteo.com <grbll@posteo.com>
2025-06-05 15:20:35 +0200

    changed healthcheck to sql query based check.

--- goapp/main.go
+++ goapp/main.go
@@ -1,10 +1,31 @@
 package main
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	var dsn string = "goapp:goapp@tcp(mysql:3306)/timestampdb"
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		log.Fatalf("DB open error: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("DB connect error: %v", err)
+	}
+
+	fmt.Println("Connection to timestampdb succesufll!")
 	for {
-		println("hi")
+		fmt.Println("no")
 		time.Sleep(2 * time.Second)
 	}
 }
--- init/02-data-structure.sql
+++ init/02-data-structure.sql
@@ -12,5 +12,3 @@ CREATE TABLE IF NOT EXISTS sessions (
   session_user INT,
   FOREIGN KEY (session_user) REFERENCES users(user_id)
 );
-
-SELECT SLEEP(10);
