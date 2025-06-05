6e14bd29dfa5fb2ce4922528c336cad169f02ab6
grbll@posteo.com <grbll@posteo.com>
2025-06-05 15:41:19 +0200

    backend adds exampluser and continues to print existing users.

--- goapp/main.go
+++ goapp/main.go
@@ -24,8 +24,27 @@ func main() {
 	}
 
 	fmt.Println("Connection to timestampdb succesufll!")
+
+	_, err = db.Exec(`INSERT INTO users (user_email) VALUES (?)`, "newuser@example.com")
+	if err != nil {
+		log.Fatalf("Insert failed: %v", err)
+	}
+
 	for {
-		fmt.Println("no")
+		rows, err := db.Query("Select * FROM users")
+		if err != nil {
+			log.Fatalf("DB query error: %v", err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var id int
+			var email string
+			if err := rows.Scan(&id, &email); err != nil {
+				log.Printf("Row scan error: %v", err)
+				continue
+			}
+			fmt.Printf("ID: %d, Email: %s\n", id, email)
+		}
 		time.Sleep(2 * time.Second)
 	}
 }
