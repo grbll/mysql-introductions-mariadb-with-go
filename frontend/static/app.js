73f63cfe810d8581ae371f6f509f604de590cd53
grbll@posteo.com <grbll@posteo.com>
2025-06-22 16:58:43 +0200

    fixed reload.

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,6 +1,15 @@
 import { login } from "./out/login.js";
 
-document.getElementById("loginBtn").addEventListener("click", () => {
-  const name = document.getElementById("name").value;
-  login(name).then(document.getElementById("message").textContent = text);
-});
+document.getElementById("loginForm").addEventListener(
+  "submit",
+  function (event) {
+    event.preventDefault(); // Prevent the browser from doing a real page reload
+
+    const name = document.getElementById("name").value;
+
+    login(name)
+      .then((text) => {
+        document.getElementById("message").textContent = text;
+      });
+  },
+);
