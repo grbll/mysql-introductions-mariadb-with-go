6c02c88574f830a83035220f7e87fa095a8e4c27
grbll@posteo.com <grbll@posteo.com>
2025-07-23 19:31:48 +0200

    version changed to use async/await syntax and json.

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,20 +1,15 @@
-console.log("app.js is loaded");
 import { login } from "./out/login.js";
-
-console.log("importet login module");
 
 document.getElementById("loginForm").addEventListener(
   "submit",
   function (event) {
     event.preventDefault(); // Prevent the browser from doing a real page reload
-    console.log("event submitted");
 
     const email = document.getElementById("email").value;
 
     login(email)
-      .then((text) => {
-        console.log("response from backend: ", text);
-        document.getElementById("message").textContent = text;
+      .then((data) => {
+        document.getElementById("message").textContent = data.message;
       });
   },
 );
--- frontend/static/out/login.js
+++ frontend/static/out/login.js
@@ -1,9 +1,21 @@
-export function login(email) {
-  return fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: "email=" + encodeURIComponent(email),
-  }).then((response) => response.text());
+export async function login(email) {
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Http error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
 }
