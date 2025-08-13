095d00f2b90096574a72eab7e4bd907a9cbf0936
grbll@posteo.com <grbll@posteo.com>
2025-08-13 18:02:47 +0200

    new backend + new click event + showing errors.

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -2,11 +2,28 @@ import { login } from "./out/login.js";
 
 document.getElementById("loginForm").addEventListener(
   "submit",
-  function (event) {
+  () => {
     event.preventDefault(); // Prevent the browser from doing a real page reload
 
     const email = document.getElementById("email").value;
+    const msg = document.getElementById("message");
 
+    login(email)
+      .then((data) => {
+        msg.textContent = data.message;
+        msg.style.color = black;
+      })
+      .catch((err) => {
+        msg.textContent = err.message;
+        msg.style.color = red;
+      });
+  },
+);
+
+document.getElementById("loginButton").addEventListener(
+  "click",
+  () => {
+    const email = document.getElementById("email").value;
     login(email)
       .then((data) => {
         document.getElementById("message").textContent = data.message;
--- frontend/static/out/login.js
+++ frontend/static/out/login.js
@@ -8,11 +8,12 @@ export async function login(email) {
       body: JSON.stringify({ email }),
     });
 
+    const result = await response.json();
+
     if (!response.ok) {
-      throw new Error(`Http error! Status: ${response.status}`);
+      throw new Error(result.error || `Http error! Status: ${response.status}`);
     }
 
-    const result = await response.json();
     return result;
   } catch (err) {
     console.error("Error:", err);
