c043083044e6551f0b60ea43361876c108244bdd
grbll@posteo.com <grbll@posteo.com>
2025-08-14 13:21:30 +0200

    new register buttton functionality and backend 0.0.61

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,5 +1,11 @@
 import { handleLogin } from "./out/login.js";
+import { handleRegister } from "./out/register.js";
 
 document.getElementById("loginForm").addEventListener("submit", handleLogin);
 
 document.getElementById("loginButton").addEventListener("click", handleLogin);
+
+document.getElementById("registerButton").addEventListener(
+  "click",
+  handleRegister,
+);
---
+++ frontend/static/out/register.js
@@ -0,0 +1,39 @@
+async function register(email) {
+  try {
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.error || `Http error! Status: ${response.status}`);
+    }
+
+    return result;
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+}
+
+export function handleRegister(event) {
+  if (event) event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const msg = document.getElementById("message");
+
+  register(email)
+    .then((data) => {
+      msg.textContent = data.message;
+      msg.style.color = "black";
+    })
+    .catch((err) => {
+      msg.textContent = err.message;
+      msg.style.color = "red";
+    });
+}
